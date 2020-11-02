d6dd0e21879f6b1093dd396826bfa56da5da3156
Callum White <callumwhite210@gmail.com>
2020-11-02 15:24:01 +1030

    readme

--- Develop/app.js
+++ Develop/app.js
@@ -66,7 +66,6 @@ async function managerQuestions(){
     ]).then( answers =>{
 
         const manager = new Manager(answers.name, answers.officeNumber, answers.id, answers.contact);
-
         teamarray.push(manager);
 
         startQuestions();
@@ -102,7 +101,9 @@ async function engineerQuestions(){
         let github = "https://github.com/${answers.github}";
         const engineer = new Engineer(answers.name, answers.id, answers.email, github);
         teamarray.push(engineer);
+
         startQuestions();
+
     }).catch(error => {
         console.log(error);
     })
@@ -133,42 +134,20 @@ async function internQuestions(){
     ]).then( answers =>{
         const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
         teamarray.push(intern);
+
         startQuestions();
+
     }).catch(error => {
         console.log(error);
     })
 };
 
-function renderHTML(){
+ function renderHTML(){
     const html = render (teamarray);
-    fs.writeFile(outputPath, html, "utf-8", function(err){
+    fs.writeFile(outputPath, html, function(err){
         if (err) throw err;
     console.log("Team page has been created!");
 });
 }
-
+ 
 startQuestions();
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
---
+++ README.md
@@ -0,0 +1,8 @@
+## Description ##
+This application runs through the terminal of Visual Studio code then prompts users to input data abou there workplace team. After all of the data is gathered the user will then select "Create HTML" on the command line. Then a HTML page will be created using all of the data gathered from user inputs.
+
+## Intructions ##
+Open the app.js file in Visual studio code then open the terminal to the Develop Directory then run a "npm install" in the terminal, this will install all of the required components for the app. After simply run a "node app.js" in the terminal to start the application. After all of the roles are filled out the user then selects "Create HTML" then view the new HTML in the output folder in the repo.        
+
+## Testing ##
+Simply run "npm test" within the terminal to starts the jest testing.
